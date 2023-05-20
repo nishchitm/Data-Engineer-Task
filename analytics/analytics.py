eaa06d03b454459a0097ee2401d6e56ff401a0e5
Your Name <you@example.com>
2023-05-20 22:53:47 +0200

    Updating the code in more concise manner.

--- analytics/analytics.py
+++ analytics/analytics.py
@@ -1,12 +1,12 @@
 import json
-import math
-import asyncio
+from math import sin, cos, acos, radians
+from geopy.distance import geodesic
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, Float, select
+from sqlalchemy.exc import OperationalError
+import pandas as pd
 from os import environ
 from time import sleep
-from sqlalchemy import create_engine
-from sqlalchemy.exc import OperationalError
-from sqlalchemy.sql import text
-from geopy.distance import geodesic
+
 
 print('Waiting for the data generator...')
 sleep(20)
@@ -17,95 +17,70 @@ while True:
         psql_engine = create_engine(environ["POSTGRESQL_CS"], pool_pre_ping=True, pool_size=10)
         break
     except OperationalError:
+        print("Error connecting PostgresSQL.")
         sleep(0.1)
 print('Connection to PostgresSQL successful.')
 
 # Write the solution here
+
 # Connect to MySQL
 while True:
     try:
         mysql_engine = create_engine(environ["MYSQL_CS"], pool_pre_ping=True, pool_size=10)
         break
     except OperationalError:
+        print("Error connecting MySQL.")
         sleep(0.1)
 print('Connection to MySQL successful.')
 
-async def etl_task():
-    while True:
-        # Pull data from PostgreSQL
-        psql_conn = psql_engine.connect()
-        psql_query = text("""
-            SELECT device_id, temperature, location, time
-            FROM devices
-            WHERE CAST(time AS TIMESTAMP) >= NOW() - INTERVAL '1 HOUR'
-        """)
-        psql_result = psql_conn.execute(psql_query).fetchall()
-        psql_conn.close()
 
-       # Perform data transformations and aggregations
-        aggregated_data = {}
-        for row in psql_result:
-            device_id = str(row['device_id'])
-            timestamp = row['time']
-            temperature = row['temperature']
-            location = json.loads(row['location'])
+def calculate_distance(lat1, lon1, lat2, lon2):
+    lat1, lon1, lat2, lon2 = map(radians, [lat1, lon1, lat2, lon2])
+    return acos(sin(lat1) * sin(lat2) + cos(lat1) * cos(lat2) * cos(lon2 - lon1)) * 6371
 
-            # Extract latitude and longitude from the location
-            latitude = location['latitude']
-            longitude = location['longitude']
 
-            # Calculate distance
-            distance = 0
-            if device_id in aggregated_data:
-                last_lat, last_lon = aggregated_data[device_id]['last_location']
+def aggregate_data(df):
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df['hour'] = df['time'].dt.floor('H')
+    df['distance'] = df['location'].apply(
+        lambda loc: calculate_distance(
+            loc['latitude'], loc['longitude'],
+            df['location'].iloc[0]['latitude'], df['location'].iloc[0]['longitude']
+        )
+    )
+    aggregated = df.groupby(['device_id', 'hour']).agg({
+        'temperature': 'max',
+        'device_id': 'count',
+        'distance': 'sum'
+    }).rename(columns={
+        'device_id': 'data_points',
+        'distance': 'total_distance'
+    }).reset_index()
+    return aggregated
 
-            if device_id in aggregated_data:
-                last_lat, last_lon = aggregated_data[device_id]['last_location']
-                last_lat = math.radians(last_lat)
-                last_lon = math.radians(last_lon)
-                distance = math.acos(
-                    math.sin(last_lat) * math.sin(latitude) +
-                    math.cos(last_lat) * math.cos(latitude) * math.cos(longitude - last_lon)
-                ) * 6371
-                
-                # distance = geodesic((last_lat, last_lon), (latitude, longitude)).kilometers           # Another method to calculate the distance
 
-            if device_id not in aggregated_data:
-                aggregated_data[device_id] = {
-                    'last_location': (latitude, longitude),
-                    'max_temperature': temperature,
-                    'data_points': 1,
-                    'total_distance': distance
-                }
-            else:
-                aggregated_data[device_id]['last_location'] = (latitude, longitude)
-                aggregated_data[device_id]['max_temperature'] = max(aggregated_data[device_id]['max_temperature'], temperature)
-                aggregated_data[device_id]['data_points'] += 1
-                aggregated_data[device_id]['total_distance'] += distance
+metadata = MetaData()
+analytics = Table(
+    'analytics', metadata,
+    Column('device_id', Integer),
+    Column('hour', Integer),
+    Column('temperature', Integer),
+    Column('data_points', Integer),
+    Column('total_distance', Float),
+)
+metadata.create_all(mysql_engine)
 
-        # Store aggregated data into MySQL
-        mysql_conn = mysql_engine.connect()
-        mysql_conn.execute(text("""
-            CREATE TABLE IF NOT EXISTS analytics (
-                device_id VARCHAR(36) NOT NULL,
-                timestamp TIMESTAMP NOT NULL,
-                max_temperature INTEGER,
-                data_points INTEGER,
-                total_distance FLOAT,
-                PRIMARY KEY (device_id, timestamp)
-            )
-        """))
+while True:
+    try:
+        with psql_engine.connect() as psql_conn, mysql_engine.connect() as mysql_conn:
 
-        for device_id, data in aggregated_data.items():
-            mysql_conn.execute(text("""
-                INSERT INTO analytics (device_id, timestamp, max_temperature, data_points, total_distance)
-                VALUES (:device_id, :timestamp, :max_temperature, :data_points, :total_distance)
-            """), device_id=device_id, timestamp=timestamp, max_temperature=data['max_temperature'],
-               data_points=data['data_points'], total_distance=data['total_distance'])
+            metadata.reflect(bind=psql_engine)
+            devices_table = metadata.tables['devices']
+            stmt = devices_table.select()
+            result = psql_conn.execute(stmt)
+            df = pd.DataFrame(result.fetchall(), columns=result.keys())
+            aggregated = aggregate_data(df)
+            mysql_conn.execute(analytics.insert(), aggregated.to_dict('records'))
 
-        mysql_conn.close()
-
-        await asyncio.sleep(3600)  # Run the task every hour
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(etl_task())
+    except OperationalError:
+        sleep(0.1)
